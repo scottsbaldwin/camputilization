239d2a1dd5f6b4eb4b67f341112f1fbca81f2887
Scott Baldwin <scottsbaldwin@gmail.com>
2013-03-26 23:04:37 -0500

    fixing algorithm for querying in camp status

--- utilization.rb
+++ utilization.rb
@@ -9,6 +9,9 @@ dates.each do |l|
   (id, arrival, departure) = l.split("\t")
   if !arrival.nil? && !departure.nil?
     idx = {
+      "id" => id,
+      "arrival" => Date.parse(arrival),
+      "departure" => Date.parse(departure),
       "arrivalWeek" => Date.parse(arrival).cweek(),
       "arrivalYear" => Date.parse(arrival).year(),
       "arrivalDay" => Date.parse(arrival).yday(),
@@ -21,30 +24,41 @@ dates.each do |l|
 end
 
 values = {}
+week_data = {}
+
+puts "\nStart\tEnd\tPeople at Camp"
 
 (2011..2013).each do |year|
   values[year] = []
   
   (1..52).each do |week|
     next if year == 2011 && week <= 23
-    next if year == 2013 && week >= 45
+    #next if year == 2013 && week >= 45
     next if year == Date.today.year && week > Date.today.cweek
-    
-    matches = index.select { |line| 
-      !line["arrivalYear"].nil? && !line["arrivalWeek"].nil? &&
-      line["arrivalYear"] <= year && line["arrivalWeek"] <= week && 
-      !line["departureYear"].nil? && !line["departureWeek"].nil? &&
-      line["departureYear"] >= year && line["departureWeek"] >= week
-    }
 
     #monday = 1
     sunday = Date.commercial(year, week, 1) - 1
     saturday = Date.commercial(year, week, 6)
     
-    puts "\nStart\tEnd\tPeople at Camp" if week % 10 == 0
+    matches = index.select { |line| 
+      !line["arrival"].nil? && !line["departure"].nil? && 
+      line["arrival"] <= saturday && line["departure"] >= sunday
+    }
+
+    week_data["#{sunday} to #{saturday}"] = matches
+
     puts "%s\t%s\t%s" % [sunday, saturday, matches.length]
     
     values[year].push matches.length
+  end
+end
+
+week_data.each do |key,value|
+  vals = value.map { |v| {"id" => v["id"], "arrival" => v["arrival"], "departure" => v["departure"]} }
+  puts "\nRaw data for #{key}"
+  puts "Counter\tID\tArrival\tDeparture"
+  vals.each_with_index do |v, i| 
+    puts "#{i+1}\t#{v['id']}\t#{v['arrival']}\t#{v['departure']}"
   end
 end
 
